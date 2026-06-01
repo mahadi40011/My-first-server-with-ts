3949e43980877dce1444cef953a93721c1ed620f
mahadi40011 <mahadi40011@gmail.com>
2026-06-01 21:03:22 +0600

    Create Product Interface and create a api for get single product from db

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { productService } from "../service/product.service";
+import type { product } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -8,25 +9,32 @@ export const productController = (
   const url = req.url;
   const method = req.method;
 
-  // const product = [
-  //   {
-  //     id: 1,
-  //     name: "Smart phone",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "laptop",
-  //   },
-  // ];
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
   const products = productService();
 
+  // get all products
   if (url === "/products" && method === "GET") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({
         message: "Products retrieved Successfully ",
         data: products,
+      }),
+    );
+  }
+
+  // get single product
+  else if (method === "GET" && id !== null) {
+    const product = products.find((p: product) => p.id === id);
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product retrieved Successfully ",
+        data: product,
       }),
     );
   }
---
+++ src/types/product.type.ts
@@ -0,0 +1,11 @@
+export interface product {
+  id: number;
+  name: string;
+  category: string;
+  brand: string;
+  price: number;
+  stock: number;
+  rating: number;
+  discount: number;
+  isAvailable: boolean;
+}
